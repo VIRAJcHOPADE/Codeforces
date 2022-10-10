9d4f5451fcee22a5e17e39c5446b70a1f81633b1
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2022-10-10 16:37:12 +0530

    Q52

---
+++ Football2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main()
+{
+  string s;
+  cin>>s;
+  int count=0;
+  for(int i=0;i<s.size()-1;i++){
+	if(s[i]==s[i+1]){
+         count++;
+		 if(count==6){
+			cout<<"YES"<<endl;
+			return 0;
+		 }
+	}
+	else{
+		count=0;
+	}
+  }
+cout<<"NO"<<endl;
+	return 0;
+}

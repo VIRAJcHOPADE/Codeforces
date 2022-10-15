09dfe02e375ca2029283df61864eef6d199f2634
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2022-10-15 18:27:48 +0530

    Q57

---
+++ Puzzles.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main() {
+	int n,m;
+	cin>>n>>m;
+	int arr[m];
+	for(int i=0;i<m;i++){
+		cin>>arr[i];
+	}
+	sort(arr,arr+m);
+	int leastval = arr[n-1]-arr[0];
+	for(int i=1;i<=m-n;i++){
+          if(arr[i+n-1]-arr[i]<leastval){
+			leastval=arr[i+n-1]-arr[i];
+		  }
+	}
+	cout<<leastval<<endl;
+	return 0;
+}
